7a2b930144d5e26f681c2f0fe031ad55ff0cc9dc
Mateusz K. <mateusz.kierman@gmail.com>
2024-06-09 14:18:09 +0200

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'LoginScreen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersive);
   await Firebase.initializeApp();
   runApp(MyApp());
 }
